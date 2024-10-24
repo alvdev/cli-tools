47c24a1a37dc5dce02fb49a1d3b99d584ae75a9b
alv.dev <soy@alv.dev>
2024-10-24 07:10:58 +0200

    add menu entry point

---
+++ add-flutter-feature/bin/cli.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'package:dcli/dcli.dart';
+import 'package:add_flutter_feature/add_config.dart';
+import 'package:add_flutter_feature/add_feature.dart';
+
+void main() {
+  if (!exists('pubspec.yaml')) {
+    print('Please run this script from the root of your Flutter project');
+    exit(0);
+  }
+
+  const options = ['Create config files', 'Create a new feature'];
+  final selected = menu(
+    'Select an option:',
+    options: options,
+  );
+
+  final isConfirmedOption = confirm(
+    "You're going to ${orange(selected.toLowerCase())}. Are you sure?",
+    defaultValue: false,
+  );
+  if (!isConfirmedOption) exit(0);
+
+  if (selected == options[0]) addConfigFiles();
+  if (selected == options[1]) addFeatureFiles();
+}
