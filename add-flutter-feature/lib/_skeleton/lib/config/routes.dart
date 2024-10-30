213366924e468b935f6cf35e2c2043fc305f5d59
alv.dev <soy@alv.dev>
2024-10-30 12:40:49 +0100

    feat: add default config routes

--- add-flutter-feature/lib/_skeleton/lib/config/routes.dart
+++ add-flutter-feature/lib/_skeleton/lib/config/routes.dart
@@ -1,10 +1,16 @@
-import 'packages:go_router/go_router.dart';
+import 'package:go_router/go_router.dart';
+import 'package:myapp/features/home/home_screen.dart';
+import 'package:myapp/features/login/login_screen.dart';
 
 final _router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
       builder: (context, state) => HomeScreen(),
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => LoginScreen(),
     ),
   ],
 );
--- add-flutter-feature/lib/_skeleton/lib/main.dart
+++ add-flutter-feature/lib/_skeleton/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:contradefensa/features/login/presentation/login_page.dart';
+import 'package:contradefensa/config/routes.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'App title',
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData.dark(),
-      home: LoginPage(),
+      routerConfig: router,
     );
   }
 }
