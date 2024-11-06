512c04d04156c55c42a17b7cb4dc6ebb2cc41165
alv.dev <soy@alv.dev>
2024-11-06 17:10:42 +0100

    add color to package installation messages

--- add-flutter-feature/lib/add_config.dart
+++ add-flutter-feature/lib/add_config.dart
@@ -14,13 +14,30 @@ void addConfigFiles() {
   }
 
   // add dependencies
-  print(cyan('Adding packages...'));
+  print(blue('Adding packages...'));
+
   'dart pub add dio'.start(detached: true);
+  print(cyan('\t-> Added Dio package'));
+
   'dart pub add envied'.start(detached: true);
+  print(cyan('\t-> Added Envied package'));
+
   'dart pub add flutter_localizations --sdk=flutter'.start(detached: true);
   'dart pub add intl'.start(detached: true);
+  print(cyan('\t-> Added Internationalization packages'));
+
   'dart pub add go_router'.start(detached: true);
+  print(cyan('\t-> Added Go Router package'));
+
   'dart pub add flutter_svg'.start(detached: true);
+  print(cyan('\t-> Added Flutter SVG package'));
+
+  'dart pub add freezed_annotation'.start(detached: true);
+  'dart pub add dev:build_runner'.start(detached: true);
+  'dart pub add dev:freezed'.start(detached: true);
+  'dart pub add add json_annotation'.start(detached: true);
+  'dart pub add dev:json_serializable'.start(detached: true);
+  print(cyan('\t-> Added Freezed packages'));
 
   // copy files
   final configFolderFrom = p.relative('lib/_skeleton');
