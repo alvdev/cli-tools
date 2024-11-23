6328025b9ec61e7f6b5b7d5f75a3268c2aa1187f
alv.dev <soy@alv.dev>
2024-11-23 09:08:22 +0100

    add toCamelCase extension to skeleton

--- add-flutter-feature/lib/_skeleton/lib/utils/extensions.dart
+++ add-flutter-feature/lib/_skeleton/lib/utils/extensions.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+// App extensions
 extension LocalizationsExtension on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this)!;
+}
+
+// Utils extensions
+extension StringExtension on String {
+  String toCamelCase() {
+    final names = '${this[0].toLowerCase()}${substring(1)}';
+    return names.replaceAll(RegExp(r'[ _-]+'), '');
+  }
 }
