4744e6bddbd2d751b0529dfb453c70f217f7e950
alv.dev <soy@alv.dev>
2024-11-24 12:51:50 +0100

    ♻️ refactor: add DRY packages

--- add-flutter-feature/lib/add_config.dart
+++ add-flutter-feature/lib/add_config.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:dcli/dcli.dart';
 import 'package:path/path.dart';
 import 'package:yaml_edit/yaml_edit.dart';
+import 'package:add_flutter_feature/add_packages.dart';
 import 'package:add_flutter_feature/replace_vars.dart';
 
 void addConfigFiles() {
@@ -45,80 +46,7 @@ void addConfigFiles() {
 
   // Add dependencies after copying skeleton. Otherwise it will fail
   print(white('\n\n⯀ Adding packages...\n'));
-
-  try {
-    'flutter pub add dio'.start(progress: Progress.devNull(), runInShell: true);
-    print(green('⯀ Added Dio package', bold: false));
-  } catch (e) {
-    print(red('⯀ Failed to add Dio package ${e.errorMessage}'));
-  }
-
-  try {
-    'flutter pub add envied'
-        .start(progress: Progress.devNull(), runInShell: true);
-    print(green('⯀ Added Envied package', bold: false));
-  } catch (e) {
-    print(red('⯀ Failed to add Envied package ${e.errorMessage}'));
-  }
-
-  try {
-    'flutter pub add flutter_localizations --sdk=flutter'
-        .start(progress: Progress.devNull(), runInShell: true);
-    'flutter pub add intl'
-        .start(progress: Progress.devNull(), runInShell: true);
-    print(green('⯀ Added Internationalization packages', bold: false));
-  } catch (e) {
-    print(red(
-        '⯀ Failed to add Internationalization packages ${e.errorMessage}'));
-  }
-
-  try {
-    'flutter pub add go_router'
-        .start(progress: Progress.devNull(), runInShell: true);
-    print(green('⯀ Added Go Router package', bold: false));
-  } catch (e) {
-    print(red('⯀ Failed to add Go Router package ${e.errorMessage}'));
-  }
-
-  try {
-    'flutter pub add flutter_riverpod'
-        .start(progress: Progress.devNull(), runInShell: true);
-    print(green('⯀ Added Flutter Riverpod package', bold: false));
-  } catch (e) {
-    print(red('⯀ Failed to add Flutter Riverpod package ${e.errorMessage}'));
-  }
-
-  try {
-    'flutter pub add flutter_svg'
-        .start(progress: Progress.devNull(), runInShell: true);
-    print(green('⯀ Added Flutter SVG package', bold: false));
-  } catch (e) {
-    print(red('⯀ Failed to add Flutter SVG package ${e.errorMessage}'));
-  }
-
-  try {
-    'flutter pub add freezed_annotation'
-        .start(progress: Progress.devNull(), runInShell: true);
-    'flutter pub add dev:build_runner'
-        .start(progress: Progress.devNull(), runInShell: true);
-    'flutter pub add dev:freezed'
-        .start(progress: Progress.devNull(), runInShell: true);
-    'flutter pub add json_annotation'
-        .start(progress: Progress.devNull(), runInShell: true);
-    'flutter pub add dev:json_serializable'
-        .start(progress: Progress.devNull(), runInShell: true);
-    print(green('⯀ Added Freezed packages', bold: false));
-  } catch (e) {
-    print(red('⯀ Failed to add Freezed packages ${e.errorMessage}'));
-  }
-
-  try {
-    'dart run build_runner build'
-        .start(progress: Progress.devNull(), runInShell: true);
-    print(green('⯀ Built Freezed classes', bold: false));
-  } catch (e) {
-    print(red('⯀ Failed to build Freezed classes: ${e.errorMessage}'));
-  }
+  addPackages();
 
   print(white('\n👍 Config files created successfully', bold: true));
   exit(0);
---
+++ add-flutter-feature/lib/add_packages.dart
@@ -0,0 +1,77 @@
+import 'package:dcli/dcli.dart';
+
+void addPackages() {
+  const Map<String, String> packages = {
+    'Dio': 'dio',
+    'Envied': 'envied',
+
+    // Internationalization
+    'Flutter Localizations': 'flutter_localizations --sdk=flutter',
+    'Internationalization': 'intl',
+
+    'Go Router': 'go_router',
+    'Flutter Riverpod': 'flutter_riverpod',
+    'Flutter SVG': 'flutter_svg',
+
+    // Freezed
+    'Freezed Annotation': 'freezed_annotation',
+    'Build Runner': 'dev:build_runner',
+    'Freezed': 'dev:freezed',
+    'JSON Annotation': 'dev:json_annotation',
+    'JSON Serializable': 'dev:json_serializable',
+
+    'Run Build Runner': 'build_runner -d',
+  };
+  final Map<String, List<String>> errors = {};
+
+  for (final pkg in packages.entries) {
+    String cmd = '';
+    String msg = '';
+    String errMsg = '';
+
+    if (pkg.key != 'Run Build Runner') {
+      cmd = 'flutter pub add ${pkg.value}';
+      msg = '⯀ Adding ${pkg.key}';
+      errMsg = '⯀ Failed to add ${pkg.value}';
+    } else {
+      cmd = 'dart run build_runner build';
+      msg = '⯀ Running Build Runner';
+      errMsg = '⯀ Failed to run Build Runner';
+    }
+
+    try {
+      cmd.start(
+          progress: Progress(devNull, captureStdout: false, stderr: (error) {
+            if (errors[pkg.key] == null) {
+              errors[pkg.key] = [];
+            }
+            errors[pkg.key]!.add(error);
+          }),
+          runInShell: true);
+      print(green(msg, bold: false));
+    } catch (e) {
+      print(red('$errMsg ${e.errorMessage}'));
+      for (String error in errors[pkg.key]!) {
+        print(red(error));
+      }
+    }
+  }
+}
+
+extension StringExtension on String {
+  String toCamelCase() {
+    final names = '${this[0].toLowerCase()}${substring(1)}';
+    return names.replaceAll(RegExp(r'[ _-]+'), '');
+  }
+}
+
+extension PackagesError on Object {
+  String get errorMessage {
+    final output = toString().split('\n');
+    output.removeAt(0); // Remove command
+    output.removeAt(0); // Remove exit code
+    final newOutput =
+        output.join().replaceAll('workingDirectory', '\nworkingDirectory');
+    return '\n$newOutput';
+  }
+}
