b256d4f0166dd605d505d93d37a19101394abcbd
alv.dev <soy@alv.dev>
2024-11-26 05:18:26 +0100

    apply custom theme to app

--- add-flutter-feature/lib/_skeleton/lib/main.dart
+++ add-flutter-feature/lib/_skeleton/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:{{ appPackageName }}/config/routes.dart';
+import 'package:{{ appPackageName }}/config/theme/theme.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       locale: const Locale('es'),
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
-      darkTheme: ThemeData.dark(),
+      darkTheme: AppTheme.dark,
       routerConfig: router,
     );
   }
